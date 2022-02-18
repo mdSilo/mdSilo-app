440bb2df3a7b1aa749fad641b0a35602ca44240e
danloh <lploh2016@gmail.com>
2022-02-18 07:17:57 -0800

    init: clean

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import '../App.css';
 import 'styles/styles.css';
 import 'styles/nprogress.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'tippy.js/dist/tippy.css';
-//import Editor from './Editor'
 import { ProvideCurrent } from 'editor/hooks/useCurrent';
 import Editor from 'components/editor/Editor';
 import { getIndexDemoEditorValue } from 'editor/constants';
@@ -14,7 +12,7 @@ const App = () => {
     <div className="flex flex-1 w-full mx-auto">
       <ProvideCurrent value={{ ty: 'note', id: '0000' }}>
         <Editor
-          className="flex-1 px-8 pt-2 pb-8 md:pb-12 md:px-12 bg-gray-800"
+          className="flex-1 p-8"
           value={getIndexDemoEditorValue()}
           setValue={() => {/*do nothing*/}}
           onChange={() => {/*do nothing*/}}
