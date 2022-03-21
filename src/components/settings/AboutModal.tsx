f1f161c207725f71bfa23a5e0ecb13aad01e85f8
danloh <lploh2016@gmail.com>
2022-03-21 08:35:24 -0700

    chore: add a check updates link

--- src/components/settings/AboutModal.tsx
+++ src/components/settings/AboutModal.tsx
@@ -1,6 +1,7 @@
 import { getVersion, getTauriVersion } from '@tauri-apps/api/app';
 import { writeText } from '@tauri-apps/api/clipboard';
 import { useState, useEffect } from 'react';
+import { openUrl } from 'file/open'
 import { BaseModal } from './BaseModal';
 
 type Props = {
@@ -33,6 +34,14 @@ export default function AboutModal({ isOpen, handleClose }: Props) {
             await writeText(`App: ${appVersion} \n Tauri: ${tauriVersion}`);
           }}>
           Copy
+        </button>
+        <button
+          className="link mt-2"
+          onClick={async () => {
+            await openUrl("https://github.com/danloh/mdSilo-app/releases");
+          }}
+        >
+          Check for Updates
         </button>
       </div>
     </BaseModal>
